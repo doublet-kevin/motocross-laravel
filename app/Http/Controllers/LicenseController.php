d4c74230b2e8fa58b2a8f241307c1b94770f4918
merylahounou97 <ahounoumeryl@yahoo.fr>
2024-01-18 10:34:19 +0100

    feat(controller): create user, license, registration and role rcontrollers

---
+++ app/Http/Controllers/LicenseController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\License;
+
+class LicenseController extends Controller
+{
+    public function index()
+    {
+        return view('license.index');
+    }
+
+    public function create()
+    {
+        return view('license.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'license_number' => 'required|string|max:255',
+            'license_type' => 'required|string|max:255',
+            'license_class' => 'required|string|max:255',
+            'license_expiration' => 'required|string|max:255',
+            'license_status' => 'required|string|max:255',
+            'license_renewal' => 'required|string|max:255',
+            'license_renewal_date'
+        ]);
+
+        $license = License::create([
+            'license_number' => $request->license_number,
+            'license_type' => $request->license_type,
+            'license_class' => $request->license_class,
+            'license_expiration' => $request->license_expiration,
+            'license_status' => $request->license_status,
+            'license_renewal' => $request->license_renewal,
+            'license_renewal_date' => $request->license_renewal_date
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $license = License::findOrFail($id);
+        return view('license.edit', ['license' => $license]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'license_number' => 'required|string|max:255',
+            'license_type' => 'required|string|max:255',
+            'license_class' => 'required|string|max:255',
+            'license_expiration' => 'required|string|max:255',
+            'license_status' => 'required|string|max:255',
+            'license_renewal' => 'required|string|max:255',
+            'license_renewal_date'
+        ]);
+
+        $license = License::findOrFail($id);
+        $license->update([
+            'license_number' => $request->license_number,
+            'license_type' => $request->license_type,
+            'license_class' => $request->license_class,
+            'license_expiration' => $request->license_expiration,
+            'license_status' => $request->license_status,
+            'license_renewal' => $request->license_renewal,
+            'license_renewal_date' => $request->license_renewal_date
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $license = License::findOrFail($id);
+        $license->delete();
+    }
+}
