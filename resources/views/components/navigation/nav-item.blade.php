8d48584b4d690cd2ddc1a10e4e0ab7cfda3244c5
kevin-d <kevin.doublet02@gmail.com>
2024-01-18 19:20:36 +0100

    refactor(layout): convert desktop navigation to blade components

---
+++ resources/views/components/navigation/nav-item.blade.php
@@ -0,0 +1,3 @@
+<li class="border border-orange-500 hover:bg-orange-500 duration-300  p-2 rounded-lg cursor-pointer">
+    <a>{{ $name }}</a>
+</li>
---
+++ resources/views/components/navigation/nav.blade.php
@@ -0,0 +1,5 @@
+<div class="container text-white items-center text-center flex justify-between mx-auto px-4">
+    <ul class="hidden lg:flex font-bold gap-x-8">
+        {{ $slot }}
+    </ul>
+</div>
