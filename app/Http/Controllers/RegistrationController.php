dab9ea76f0e910b4526e9d4e17d76bc6f9b8460d
merylahounou97 <ahounoumeryl@yahoo.fr>
2024-01-18 10:34:19 +0100

    feat(controller): create user, license, registration and role rcontrollers

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Registration;
+
+class RegistrationController extends Controller
+{
+    public function index()
+    {
+        return view('registration.index');
+    }
+
+    public function create()
+    {
+        return view('registration.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'firstname' => 'required|string|max:255',
+            'lastname' => 'required|string|max:255',
+            'region' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'postal_code' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'birth_date' => 'required|string|max:255',
+            'password' => 'required|string|max:255',
+            'role' => 'required|string|max:255',
+        ]);
+
+        $registration = Registration::create([
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'region' => $request->region,
+            'city' => $request->city,
+            'postal_code' => $request->postal_code,
+            'email' => $request->email,
+            'birth_date' => $request->birth_date,
+            'password' => $request->password,
+            'role' => $request->role,
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $registration = Registration::findOrFail($id);
+        return view('registration.edit', ['registration' => $registration]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'firstname' => 'required|string|max:255',
+            'lastname' => 'required|string|max:255',
+            'region' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'postal_code' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'birth_date' => 'required|string|max:255',
+            'password' => 'required|string|max:255',
+            'role' => 'required|string|max:255',
+        ]);
+
+        $registration = Registration::findOrFail($id);
+        $registration->update([
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'region' => $request->region,
+            'city' => $request->city,
+            'postal_code' => $request->postal_code,
+            'email' => $request->email,
+            'birth_date' => $request->birth_date,
+            'password' => $request->password,
+            'role' => $request->role,
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $registration = Registration::findOrFail($id);
+        $registration->delete();
+    }
+
+    public function show($id)
+    {
+        $registration = Registration::findOrFail($id);
+        return view('registration.show', ['registration' => $registration]);
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return view('user.index');
+    }
+
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'firstname' => 'required|string|max:255',
+            'lastname' => 'required|string|max:255',
+            'region' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'postal_code' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'birth_date' => 'required|string|max:255',
+            'password' => 'required|string|max:255',
+            'role' => 'required|string|max:255',
+        ]);
+
+        $user = User::create([
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'region' => $request->region,
+            'city' => $request->city,
+            'postal_code' => $request->postal_code,
+            'email' => $request->email,
+            'birth_date' => $request->birth_date,
+            'password' => $request->password,
+            'role' => $request->role,
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('user.edit', ['user' => $user]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'firstname' => 'required|string|max:255',
+            'lastname' => 'required|string|max:255',
+            'region' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'postal_code' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'birth_date' => 'required|string|max:255',
+            'password' => 'required|string|max:255',
+            'role' => 'required|string|max:255',
+        ]);
+
+        $user = User::findOrFail($id);
+        $user->update([
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'region' => $request->region,
+            'city' => $request->city,
+            'postal_code' => $request->postal_code,
+            'email' => $request->email,
+            'birth_date' => $request->birth_date,
+            'password' => $request->password,
+            'role' => $request->role,
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return view('user.show', ['user' => $user]);
+    }
+}
