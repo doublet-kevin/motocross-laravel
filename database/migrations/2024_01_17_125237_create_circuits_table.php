4df11f0ad475af789c6e2e8033bc9555b39c8af0
RtoipB <bursztyka.piotr@gmail.com>
2024-01-19 09:43:42 +0100

    fix(controller/routes): fix merge conflict

--- database/migrations/2024_01_17_125237_create_circuits_table.php
+++ database/migrations/2024_01_17_125237_create_circuits_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('circuits', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('id_club')->constrained(table: 'clubs')->onDelete('cascade');
+            $table->foreignId('id_club')->constrained(table: 'clubs');
         });
     }
 
