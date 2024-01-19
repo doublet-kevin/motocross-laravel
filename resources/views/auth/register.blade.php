7b6d648ccb8ff6b819bd35622fb63fcc9ee518af
Kévin D <kevin.doublet02@gmail.com>
2024-01-19 16:42:16 +0100

    feat(view): register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,15 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+@section('content')
+    <div class="flex justify-around max-w-xl border rounded-sm m-auto">
+        <div class="flex flex-col gap-4 justify-around max-w-80 p-8 rounded-md">
+            <h2>Inscrivez-vous</h2>
+            <form action="{{ route('register') }}" method="POST" class="flex  flex-col gap-2">
+                @csrf
+                <input type="text" name="lastname" id="lastname" required placeholder="Nom"
+                    class="bg-transparent border rounded text-sm px-1.5 py-1" />
+                <input type="text" name="firstname" id="firstname" required placeholder="Prénom"
+                    class="bg-transparent border rounded text-sm px-1.5 py-1" />
+                <input type="text" name="firstname" id="firstname" required placeholder="Prénom"
+                    class="bg-transparent border rounded text-sm px-1.5 py-1" />
+                <input type="email" name="email" id="email" required placeholder="Adresse mail"
+                    class="bg-transparent border rounded text-sm px-1.5 py-1" />
+                <input type="password" name="password" id="password" placeholder="Mot de passe" required
+                    class="bg-transparent border rounded text-sm px-1.5 py-1" />
+                <input type="password" name="password_confirmation" id="password_confirmation"
+                    placeholder="Confirmez votre mot de passe" required
+                    class="bg-transparent border rounded text-sm px-1.5 py-1" />
+                <div>
+                    <button type="submit" class="flex">Inscription</button>
+                </div>
+            </form>
+        </div>
 
-<body>
-    Register View
-</body>
-
-</html>
+    </div>
+@endsection
