1278a04ce01e6219f8bb4f494751b99b378a1908
kevin-d <kevin.doublet02@gmail.com>
2024-01-20 17:55:24 +0100

    feat(view): adjust home header section and make it responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,53 +3,24 @@
 
 @section('content')
     <div class="p-2 lg:p-0">
-
-        <div class="lg:grid lg:grid-cols-2  min-h-screen">
-            <div class="col-span-1">
-                <div>
-                    <div class="rounded-full mt-8">
-                        <img width="800px" height="800px" class="bg-motocross-circle" />
-                    </div>
-                </div>
-            </div>
-            <div class="col-span-1">
-                <main class="container text-center lg:text-start mx-auto px-4 py-8 items-center lg:flex lg:flex-col">
-
-                    <h1
-                        class="mt-1
-                                bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-4xl
-                                font-extrabold uppercase tracking-tighter text-transparent sm:text-5xl lg:text-7xl">
-                        Motocross
-                    </h1>
-                    <h1
-                        class="mt-1 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-4xl font-extrabold uppercase tracking-tighter text-transparent sm:text-5xl lg:text-7xl">
-                        AURIBAIL
-                    </h1>
-                    <div
-                        class="max-w-sm p-6 mt-10 card-glass bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <a href="#">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                Noteworthy technology acquisitions 2021</h5>
-                        </a>
-                        <p class="mb-3 font-normal text-gray-200 dark:text-gray-400">Lorem ipsum dolor sit amet,
-                            consectetur adipiscing elit. Sed euismod,
-                            nunc id ullamcorper aliquam, dui mauris aliquet justo, nec tincidunt
-                            nunc nunc id lectus.
-                        </p>
-                        <a href=""
-                            class="bg-orange-500 !text-white hover:bg-orange-600 duration-500 inline-flex items-center px-3 py-2 text-sm font-medium text-center rounded-lg  focus:ring-4 focus:outline-none ">
-                            Learn more
-                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M1 5h12m0 0L9 1m4 4L9 9" />
-                            </svg>
-                        </a>
-                    </div>
-                </main>
-            </div>
+        <div class="hidden lg:grid grid-cols-[repeat(auto-fit,minmax(100px,_1fr))] max-w-7xl m-auto my-8">
+            <img class="header-img" src="{{ Vite::asset('resources/images/moto-header-1.jpg') }}" alt="Image 1">
+            <img class="header-img" src="{{ Vite::asset('resources/images/moto-header-2.jpg') }}" alt="Image 2">
+            <img class="header-img" src="{{ Vite::asset('resources/images/moto-header-3.jpg') }}" alt="Image 3">
+            <img class="header-img" src="{{ Vite::asset('resources/images/moto-header-4.jpg') }}" alt="Image 4">
+            <img class="header-img" src="{{ Vite::asset('resources/images/moto-header-5.png') }}" alt="Image 4">
         </div>
-        <div class="w-full mb-8 flex flex-col lg:flex-row justify-center items-center gap-x-10 p-2 lg:px-8 ">
+        <div
+            class="grid lg:grid-cols-2 max-w-6xl font-bold text-light gap-8 my-20 text-center lg:text-start mx-6 lg:mx-auto m-auto">
+            <h1 class="text-4xl">
+                Commencer l'aventure maintenant en rejoignant le club motocross
+                <span class="text-primary">d'Auribail</span>
+                !
+            </h1>
+            <h1 class="text-4xl">Les prochains entrainements</h1>
+
+        </div>
+        <div class="w-full mb-8 flex flex-col lg:flex-row justify-center items-center gap-x-10 p-2 lg:px-4">
             <x-home.card title="technology acquisitions 2021" button="Learn more" link="/">
                 Lorem ipsum dolor sit amet,
                 consectetur adipiscing elit. Sed euismod,
