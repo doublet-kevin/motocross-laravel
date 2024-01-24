a27ac258cb9568efd68bb9efdbf25641043f1460
kevin-d <kevin.doublet02@gmail.com>
2024-01-24 21:14:48 +0100

    feat(view): [WIP] responsive dashboard

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,10 +1,19 @@
 @extends('admin.layout')
 
 @section('content')
-    <div class="flex flex-col gap-4">
-        @foreach ($users as $user)
-            <div class="grid grid-cols-2 gap-4 sm:grid-cols-4 md:grid-cols-9">
-                <div class="flex gap-2">
+    <div class="flex flex-col gap-4 mx-4 lg:mx-0">
+        <div class="grid grid-cols-2 md:grid-cols-6 lg:grid-cols-9">
+            <span class="text-center">Actions</span>
+            <span class="hidden sm:flex">Nom</span>
+            <span class="hidden sm:flex">Prénom</span>
+            <span>Adresse mail</span>
+            <span class="hidden md:flex">Numéro de license</span>
+            <span class="hidden lg:flex">Date de naissance</span>
+            <span class="hidden md:flex">Région</span>
+            <span class="hidden lg:flex">Ville</span>
+            <span class="hidden lg:flex">Code postal</span>
+            @foreach ($users as $user)
+                <div class="flex justify-center gap-2 py-2">
                     <x-admin.board.actions route="{{ route('user.destroy', $user->id) }}"
                         icon="{{ Vite::asset('resources/images/icons/trash.svg') }}" alt="Trash" />
 
@@ -21,11 +30,10 @@
                 <span class="hidden md:flex">{{ $user->license_number }}</span>
                 <span class="hidden lg:flex">{{ $user->birth_date }}</span>
                 <span class="hidden md:flex">{{ $user->region }}</span>
-                <span class="hidden md:flex">{{ $user->city }}</span>
-                <span class="hidden md:flex">{{ $user->postal_code }}</span>
-
-            </div>
-        @endforeach
+                <span class="hidden lg:flex">{{ $user->city }}</span>
+                <span class="hidden lg:flex">{{ $user->postal_code }}</span>
+            @endforeach
+        </div>
         <button onclick="window.location.href='{{ route('user.create') }}'"
             class="flex justify-center col-span-3 button whitespace-nowrap">Ajouter un utilisateur</button>
     </div>
