81026f096b501b1578b1dc49f066477ab7f3a058
kevin-d <kevin.doublet02@gmail.com>
2024-01-27 17:45:37 +0100

    feat(view): adjust responsive layout admin

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -38,21 +38,40 @@
             </div>
             <!-- Desktop Navigation -->
             <x-navigation.mobile-nav />
-
-            <!-- Admin Dashboard Navigation -->
-            <ul class="flex justify-center gap-4 py-4">
-                <li><a href="{{ route('admin.users.index') }}">Utilisateurs</a></li>
-                <li><a href="{{ route('admin.trainings.index') }}">Entraînements</a></li>
-                <li><a href="{{ route('admin.circuits.index') }}">Circuits</a></li>
-                <li><a href="{{ route('admin.licenses.index') }}">Licenses</a></li>
-            </ul>
         </header>
+
+
         <main class="flex items-center justify-center flex-grow w-full max-w-5xl mx-auto mt-8 xl:max-w-7xl">
+            <!-- Admin Dashboard Navigation -->
             <div class="flex-grow">
+                <ul class="hidden gap-4 py-4 mx-4 sm:flex lg:mx-0">
+                    <li><a href="{{ route('admin.users.index') }}" class="button-inactive">Utilisateurs</a></li>
+                    <li><a href="{{ route('admin.trainings.index') }}" class="button-inactive">Entraînements</a></li>
+                    <li><a href="{{ route('admin.circuits.index') }}" class="button-inactive">Circuits</a></li>
+                    <li><a href="{{ route('admin.licenses.index') }}" class="button-inactive">Licenses</a></li>
+                </ul>
+
+                <div x-data="{ open: false }" class="flex flex-col m-4 sm:hidden lg:mx-0">
+                    <button @click="open = !open"
+                        class="flex items-center justify-center gap-2 px-3 py-2 font-medium text-center duration-500 bg-orange-500 rounded-lg hover:bg-orange-600 focus:ring-4 focus:outline-none">
+                        <span>Dashboard</span>
+                        <img src="{{ Vite::asset('resources/images/icons/chevron-down.svg') }}"
+                            class="transition-all duration-300"
+                            x-bind:style="open ? 'transform: rotate(-180deg);;' : 'transform: rotate(0deg);'">
+                    </button>
+                    <ul class="flex flex-col gap-2 overflow-hidden transition-all duration-300 max-h-0"
+                        x-bind:style="open ? 'max-height: 500px;' : 'max-height: 0;'">
+                        <li><a href="{{ route('admin.users.index') }}" class="mt-2 button-inactive">Utilisateurs</a>
+                        </li>
+                        <li><a href="{{ route('admin.trainings.index') }}" class="button-inactive">Entraînements</a>
+                        </li>
+                        <li><a href="{{ route('admin.circuits.index') }}" class="button-inactive">Circuits</a></li>
+                        <li><a href="{{ route('admin.licenses.index') }}" class="button-inactive">Licenses</a></li>
+                    </ul>
+                </div>
+
                 @yield('content')
             </div>
-
-
         </main>
         <x-layout.footer>
             <x-layout.footer-item title="Find us on..">
