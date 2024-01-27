dd9b3e3eca86df1d93449c5c70642bbe2a60d68f
kevin-d <kevin.doublet02@gmail.com>
2024-01-27 16:14:11 +0100

    feat(refactor): refactor board admin user view

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Motocross</title>
-    @vite(['resources/css/app.css', 'resources/css/admin-dashboard.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('styles')
     @stack('scripts')
 </head>
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,63 +1,64 @@
 @extends('admin.layout')
-
 @section('content')
     <div class="flex flex-col gap-4 mx-4 lg:mx-0">
-        <div
-            class="grid grid-cols-3 border rounded-lg divide-primary sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-10 border-primary">
-            <div class="flex justify-center border-r rounded-tl-lg col-title">
-                <p>Actions</p>
-            </div>
-            <div class="hidden border-r sm:flex col-title">
-                <p>Nom</p>
-            </div>
-            <div class="hidden border-r sm:flex col-title">
-                <p>Prénom</p>
-            </div>
-            <div class="col-span-2 border-r rounded-tr-lg md:rounded-tr-none col-title">
-                <p>
-                    Adresse mail
-                </p>
-            </div>
-            <div class="hidden lg:border-r md:flex col-title md:rounded-tr-lg lg:rounded-tr-none">
-                <p>License</p>
-            </div>
-            <div class="hidden border-r lg:flex col-title">
-                <p>
-                    Date de naissance
-                </p>
-            </div>
-            <div class="hidden lg:flex lg:border-r col-title">
-                <p>Région</p>
-            </div>
-            <div class="hidden border-r lg:flex col-title">
-                <p>Ville</p>
-            </div>
-            <div class="hidden lg:flex lg:rounded-tr-lg col-title">
-                <p>
-                    Code postal
-                </p>
-            </div>
-            @foreach ($users as $user)
-                <div class="flex items-center justify-center gap-2 py-2 border-r">
-                    <x-admin.board.actions route="{{ route('user.destroy', $user->id) }}"
-                        icon="{{ Vite::asset('resources/images/icons/trash.svg') }}" alt="Trash" />
+        <div class="relative overflow-x-auto border rounded-lg border-primary">
+            <table class="w-full text-sm truncate">
+                <thead class="text-gray-700 uppercase bg-secondary">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">Actions</th>
+                        <th scope="col" class="px-6 py-3">Adresse mail</th>
+                        <th scope="col" class="hidden px-6 py-3 sm:table-cell">Nom</th>
+                        <th scope="col" class="hidden px-6 py-3 sm:table-cell">Prénom</th>
+                        <th scope="col" class="hidden px-6 py-3 md:table-cell">Licence</th>
+                        <th scope="col" class="hidden px-6 py-3 md:table-cell">Date de naissance</th>
+                        <th scope="col" class="hidden px-6 py-3 lg:table-cell">Région</th>
+                        <th scope="col" class="hidden px-6 py-3 lg:table-cell">Ville</th>
+                        <th scope="col" class="hidden px-6 py-3 xl:table-cell">Code postal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr class="border-b last:border-b-0 border-primary odd:bg-transparent">
+                            <th scope="row" class="px-6 py-4 font-medium border-r border-primary whitespace-nowrap">
+                                <div class="flex items-center justify-center gap-2">
+                                    <x-admin.board.actions route="{{ route('user.destroy', $user->id) }}"
+                                        icon="{{ Vite::asset('resources/images/icons/trash.svg') }}" alt="Trash" />
 
-                    <x-admin.board.actions route="{{ route('user.edit', $user->id) }}"
-                        icon="{{ Vite::asset('resources/images/icons/edit.svg') }}" alt="Edit" />
+                                    <x-admin.board.actions route="{{ route('user.edit', $user->id) }}"
+                                        icon="{{ Vite::asset('resources/images/icons/edit.svg') }}" alt="Edit" />
 
-                    <x-admin.board.actions route="{{ route('user.show', $user->id) }}"
-                        icon="{{ Vite::asset('resources/images/icons/eye.svg') }}" alt="Show" />
-                </div>
-
-                <span class="hidden border-r sm:flex col-content">{{ $user->firstname }}</span>
-                <span class="hidden border-r sm:flex col-content">{{ $user->lastname }}</span>
-                <span class="flex col-span-2 md:border-r col-content">{{ $user->email }}</span>
-                <span class="hidden lg:border-r md:flex col-content">{{ $user->license_number }}</span>
-                <span class="hidden border-r lg:flex col-content">{{ $user->birth_date }}</span>
-                <span class="hidden border-r lg:flex col-content">{{ $user->region }}</span>
-                <span class="hidden p-2 border-r lg:flex col-content">{{ $user->city }}</span>
-                <span class="hidden lg:flex col-content">{{ $user->postal_code }}</span>
-            @endforeach
+                                    <x-admin.board.actions route="{{ route('user.show', $user->id) }}"
+                                        icon="{{ Vite::asset('resources/images/icons/eye.svg') }}" alt="Show" />
+                                </div>
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $user->email }}
+                            </td>
+                            <td class="hidden px-6 py-4 sm:table-cell">
+                                {{ $user->firstname }}
+                            </td>
+                            <td class="hidden px-6 py-4 sm:table-cell">
+                                {{ $user->lastname }}
+                            </td>
+                            <td class="hidden px-6 py-4 md:table-cell">
+                                {{ $user->license_number }}
+                            </td>
+                            <td class="hidden px-6 py-4 md:table-cell">
+                                {{ $user->birth_date }}
+                            </td>
+                            <td class="hidden px-6 py-4 lg:table-cell">
+                                {{ $user->region }}
+                            </td>
+                            <td class="hidden px-6 py-4 lg:table-cell">
+                                {{ $user->city }}
+                            </td>
+                            <td class="hidden px-6 py-4 xl:table-cell">
+                                {{ $user->postal_code }}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
         <button onclick="window.location.href='{{ route('user.create') }}'"
             class="flex justify-center col-span-3 button whitespace-nowrap">Ajouter un utilisateur</button>
