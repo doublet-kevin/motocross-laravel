0f58dc28fbb65ce2cb40a783596dddd1b25776a2
pbursztyka <piotr.bursztyka@etudiant.univ-rennes1.fr>
2024-02-04 16:15:03 +0100

    fix: delete ->after('name') in migration file 2024_01_17_125237_create_circuits_table.php

--- database/migrations/2024_01_17_125237_create_circuits_table.php
+++ database/migrations/2024_01_17_125237_create_circuits_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('circuits', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description')->nullable()->after('name');
+            $table->text('description')->nullable();
         });
     }
 
