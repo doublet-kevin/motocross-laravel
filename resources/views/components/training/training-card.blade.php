373fed83754bfd7ab2e5e20c4129610ca620649c
kevin-d <kevin.doublet02@gmail.com>
2024-02-04 19:51:31 +0100

    feat(view): adjust responsive view for trainings list

--- resources/views/components/training/training-card.blade.php
+++ resources/views/components/training/training-card.blade.php
@@ -1,19 +1,23 @@
-<div class="flex flex-col border rounded">
-    <img src="{{ $circuitImg }}" alt="circuit" class="object-cover w-[300px] h-[100px]">
-    <span class="text-xl font-bold text-primary">{{ $training->circuit->name }}</span>
-        <div class="flex items-center gap-2">
-            {{ date('d/m/Y', strtotime($training->date))}}
-            <img src='{{ Vite::asset("resources/images/icons/calendar.svg") }}' alt="calendar" class="w-4 h-4">
-        </div>
+<div class="flex flex-col overflow-hidden border rounded-md border-primary backdrop-blur-md shrink-0">
+    <img src="{{ $circuitImg }}" alt="circuit" class="object-cover w-[300px] h-[100px] rounded-t-md border-b border-primary">
+    <div class="flex flex-col p-2">
+        <span class="text-xl font-bold text-primary">{{ $training->circuit->name }}</span>
+        <div class="flex justify-between gap-4 pb-2">
             <div class="flex items-center gap-2">
-            {{ date('H:i', strtotime($training->date))}}
-            <img src='{{ Vite::asset("resources/images/icons/clock.svg") }}' alt="clock" class="w-4 h-4">
-        </div>
+                {{ date('d/m/Y', strtotime($training->date))}}
+                <img src='{{ Vite::asset("resources/images/icons/calendar.svg") }}' alt="calendar" class="w-4 h-4">
+            </div>
+                <div class="flex items-center gap-2">
+                {{ date('H:i', strtotime($training->date))}}
+                <img src='{{ Vite::asset("resources/images/icons/clock.svg") }}' alt="clock" class="w-4 h-4">
+            </div>
 
-        <div class="flex items-center gap-2">
-            {{ $training->number_of_places}}/75
-            <img src='{{ Vite::asset("resources/images/icons/users.svg") }}' alt="users" class="w-5 h-5">
+            <div class="flex items-center gap-2">
+                {{ $training->number_of_places}}/75
+                <img src='{{ Vite::asset("resources/images/icons/users.svg") }}' alt="users" class="w-5 h-5">
+            </div>
         </div>
-        <button class="button">Participer</button>
-    {{ $training->type }}
+            <button class="button">Participer</button>
+
+    </div>
 </div>
--- resources/views/training/index.blade.php
+++ resources/views/training/index.blade.php
@@ -1,22 +1,33 @@
 @extends('layout')
 @section('title')
-    <h1 class="text-4xl font-bold text-center">Inscrivez-vous sur les prochains <span class="text-primary">entraînements</span></h1>
+    <h1 class="text-4xl font-bold text-center">Inscrivez-vous sur les prochains <span
+            class="text-primary">entraînements</span></h1>
 @endsection
 @section('content')
-    <div class="flex">
-        <h2 class="text-2xl font-bold text-primary">Pour nos pilotes chevronnés</h2>
-    </div>
-    <div class="grid grid-cols-3 gap-4">
-        @foreach ($trainings as $training)
-         <x-training.training-card :training="$training" circuitImg="{{ Vite::asset('resources/images/circuit-1.jpg') }}"/>
-        @endforeach
-    </div>
-        <div class="flex">
-        <h2 class="text-2xl font-bold text-primary">Pour nos jeunes pilotes*</h2>
-    </div>
-    <div class="grid grid-cols-3 gap-4">
-        @foreach ($trainings as $training)
-         <x-training.training-card :training="$training" circuitImg="{{ Vite::asset('resources/images/circuit-1.jpg') }}"/>
-        @endforeach
+    <div class="flex flex-col items-center justify-start gap-2">
+        <div class="flex flex-col gap-2">
+            <h2 class="py-2 text-2xl font-bold underline text-primary">Pour nos pilotes chevronnés</h2>
+            <div class="flex flex-col items-center justify-center">
+                <div class="flex w-[350px] md:w-auto gap-4 overflow-scroll">
+                    @foreach ($trainings as $training)
+                        <x-training.training-card :training="$training"
+                            circuitImg="{{ Vite::asset('resources/images/circuit-1.jpg') }}" />
+                    @endforeach
+                </div>
+            </div>
+        </div>
+
+        <div class="flex flex-col gap-2">
+            <h2 class="py-2 text-2xl font-bold underline text-primary">Pour nos jeunes pilotes*</h2>
+            <div class="flex w-[350px] md:w-auto gap-4 overflow-scroll">
+                @foreach ($trainings as $training)
+                    <x-training.training-card :training="$training"
+                        circuitImg="{{ Vite::asset('resources/images/circuit-1.jpg') }}" />
+                @endforeach
+            </div>
+            <span class="flex py-2 w-80">(*) Les sessions jeunes pilotes sont accessibles pour les 12-18
+                ans</span>
+        </div>
+
     </div>
 @endsection
