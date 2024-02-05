785ec9308f9d23d785af19e881131c6d566420d8
Kévin D <kevin.doublet02@gmail.com>
2024-02-05 15:00:07 +0100

    feat(view): add show training view

--- resources/views/training/show.blade.php
+++ resources/views/training/show.blade.php
@@ -3,9 +3,18 @@
     <h1 class="text-4xl font-bold text-center">Liste des <span class="text-primary">pilotes</span></h1>
 @endsection
 @section('content')
-    <div class="">
-        @foreach ($participants as $participant)
-            <div>{{ $participant->firstname }} {{ $participant->lastname }}</div>
-        @endforeach
+    <div class="max-w-4xl py-8">
+        <div class="grid grid-cols-8 gap-4">
+            @foreach ($participants as $participant)
+                <button class="button-inactive backdrop-blur-lg">
+                    {{ $participant->firstname }} {{ $participant->lastname }}
+                </button>
+            @endforeach
+            @for ($i = 1; $i <= 75 - $participants->count(); $i++)
+                <div class="p-1 text-center rounded border border-primary backdrop-blur-lg">
+                    Place disponible
+                </div>
+            @endfor
+        </div>
     </div>
 @endsection
