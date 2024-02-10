0d94ea161c8ca729bd37c32f75de9c48e2dc57e9
kevin-d <kevin.doublet02@gmail.com>
2024-02-10 14:44:51 +0100

    feat(profil): [WIP] user profile

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,50 +1,44 @@
 @extends('layout')
 @section('title')
-    <h1 class="text-4xl font-bold text-center">Fiche pilote - <span class="text-primary">{{ $user->firstname }}
-            {{ $user->lastname }}</span></h1>
+    <div class="flex gap-4 pb-4">
+        <h1 class="text-4xl font-bold text-center">Fiche pilote - <span class="text-primary">{{ $user->firstname }}
+                {{ $user->lastname }}</span></h1>
+        @auth
+            @if (Auth::id() === $user->id)
+                <a href="{{ route('user.edit', Auth::id()) }}" class="button-inactive">Modifier mes informations personnel</a>
+            @endauth
+        @endauth
+</div>
 @endsection
 @push('styles')
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
 @endpush
 @push('scripts')
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 @endpush
 @section('content')
-    <div class="flex flex-col border rounded border-primary">
-        <span> {{ 'Prénom: ' . $user->firstname }}</span>
-        <span>{{ 'Nom: ' . $user->lastname }}</span>
-        @foreach ($trainingsHistory as $trainingHistory)
-            <span>{{ 'Date: ' . $trainingHistory->date }}</span>
-            <span>{{ 'Heure: ' . $trainingHistory->hour }}</span>
-            <span>{{ 'Circuit: ' . $trainingHistory->circuit->name }}</span>
-        @endforeach
-    </div>
-
-
-    {{ 'Id club: ' . $user->id_club }}
-    {{ 'Id licence: ' . $user->id_license }}
-    {{ 'Région: ' . $user->region }}
-    {{ 'Ville: ' . $user->city }}
-    {{ 'Code postal: ' . $user->postal_code }}
-    {{ 'Email: ' . $user->email }}
-    {{ 'Date: ' . $user->birth_date }}
-    @if (session('message'))
-        {{ session('message') }}
-        <script>
-            Toastify({
-                text: {!! json_encode(session('message')) !!},
-                duration: 3000,
-                gravity: "top",
-                position: "right",
-                offset: {
-                    x: 20,
-                    y: '80vh'
-                },
-                style: {
-                    background: "linear-gradient(to right, #00b09b, #96c93d)",
-                },
-                onClick: function() {}
-            }).showToast();
-        </script>
-    @endif
+<div>
+    <h2 class="text-3xl font-bold">Entraînement effectuer</h2>
+</div>
+<div>
+    <h2 class="text-3xl font-bold">Entraînement à venir</h2>
+</div>
+@if (session('message'))
+    <script>
+        Toastify({
+            text: {!! json_encode(session('message')) !!},
+            duration: 3000,
+            gravity: "top",
+            position: "right",
+            offset: {
+                x: 20,
+                y: '80vh'
+            },
+            style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+            },
+            onClick: function() {}
+        }).showToast();
+    </script>
+@endif
 @endsection
