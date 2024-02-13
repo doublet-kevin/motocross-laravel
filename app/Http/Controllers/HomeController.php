8a1d2ca7d0e70866cbf249b60db180403b828d0d
RtoipB <bursztyka.piotr@gmail.com>
2024-02-13 09:57:34 +0100

    features(HomeController): max_participants add to next senior and junior training's card

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Training;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -10,16 +11,22 @@ class HomeController extends Controller
     {
         $nextJuniorTraining = Training::select(
             'trainings.*',
+            DB::raw('COUNT(registrations.id) as occupied_places')
         )
+            ->leftJoin('registrations', 'trainings.id', '=', 'registrations.training_id')
             ->where('type', 'Jeune pilote')
+            ->groupBy('trainings.id', 'trainings.type', 'trainings.date', 'trainings.max_participants', 'trainings.circuit_id')
             ->orderBy('trainings.date', 'desc')
             ->first();
 
 
         $nextSeniorTraining = Training::select(
             'trainings.*',
+            DB::raw('COUNT(registrations.id) as occupied_places')
         )
+            ->leftJoin('registrations', 'trainings.id', '=', 'registrations.training_id')
             ->where('type', 'Pilote senior')
+            ->groupBy('trainings.id', 'trainings.type', 'trainings.date', 'trainings.max_participants', 'trainings.circuit_id')
             ->orderBy('trainings.date', 'desc')
             ->first();
 
